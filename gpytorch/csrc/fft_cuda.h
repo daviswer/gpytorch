726e5458261df4ff40a3d1808c45592c502e8e11
daviswert <dww78@cornell.edu>
2017-09-29 19:05:26 -0400

    Added 2D R2C fn

--- gpytorch/csrc/fft_cuda.h
+++ gpytorch/csrc/fft_cuda.h
@@ -1,2 +1,3 @@
 int fft1_r2c_cuda(THCudaTensor *input, THCudaTensor *output);
+int fft2_r2c_cuda(THCudaTensor *input, THCudaTensor *output);
 int fft1_c2r_cuda(THCudaTensor *input, THCudaTensor *output);
