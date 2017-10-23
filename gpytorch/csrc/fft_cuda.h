6eb3bcff5879ca2659fbdb45ce3759bba82a2bd0
daviswert <dww78@cornell.edu>
2017-10-23 15:02:26 -0400

    Added 3d and 2d_c fns

--- gpytorch/csrc/fft_cuda.h
+++ gpytorch/csrc/fft_cuda.h
@@ -1,4 +1,9 @@
 int fft1_r2c_cuda(THCudaTensor *input, THCudaTensor *output);
 int fft2_r2c_cuda(THCudaTensor *input, THCudaTensor *output);
+int fft3_r2c_cuda(THCudaTensor *input, THCudaTensor *output);
+int fft2_c2c_cuda(THCudaTensor *input, THCudaTensor *output);
+
 int fft1_c2r_cuda(THCudaTensor *input, THCudaTensor *output);
 int fft2_c2r_cuda(THCudaTensor *input, THCudaTensor *output);
+int fft3_c2r_cuda(THCudaTensor *input, THCudaTensor *output);
+int fft2_c2c_cuda(THCudaTensor *input, THCudaTensor *output);
