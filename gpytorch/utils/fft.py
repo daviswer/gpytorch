59accd2660af170180105241d260868a64d6ad62
daviswert <dww78@cornell.edu>
2017-10-23 15:01:40 -0400

    Added 3d transform fns

--- gpytorch/utils/fft.py
+++ gpytorch/utils/fft.py
@@ -41,6 +41,26 @@ def fft2(input):
         output_size = [n, (d // 2) + 1, 2]
     return output.view(*output_size).type(orig_type)
 
+def fft3(input):
+    # [..., m n d]
+    orig_size = input.size()
+    orig_type = type(input)
+    
+    input = input.view(-1, input.size(-3), input.size(-2), input.size(-1))
+    nPlanes, m, n, d = input.size()
+    
+    output = input.new().resize_(nPlanes, m, n, (d // 2) + 1, 2)
+    if input.is_cuda:
+        libfft.fft3_r2c_cuda(input, output)
+    else:
+        assert False
+    
+    if len(orig_size) > 3:
+        output_size = list(orig_size[:-3]) + [m, n, (d // 2) + 1, 2]
+    else:
+        output_size = [m, n, (d // 2) + 1, 2]
+    return output.view(*output_size).type(orig_type)
+
 def fft2_c(input):
     # [..., n d 2]
     orig_size = input.size()
@@ -102,6 +122,30 @@ def ifft2(input, size=None):
     else:
         assert False
     output.div_(n*d)
+    return output.view(size).type(orig_type)
+
+def ifft3(input, size=None):
+    # [..., m, n, d, 2]
+    orig_type = type(input)
+
+    if not size:
+        size = list(input.size())[:-1]
+        d = (size[-1] - 1) * 2
+        size[-1] = d
+        n = size[-2]
+        m = size[-3]
+    else:
+        d = size[-1]
+        n = size[-2]
+        m = size[-3]
+    input = input.view(-1, *input.size()[-4:])
+
+    output = input.new().resize_(input.size(0), m, n, d)
+    if input.is_cuda:
+        libfft.fft3_c2r_cuda(input, output)
+    else:
+        assert False
+    output.div_(m*n*d)
     return output.view(size).type(orig_type)
 
 def ifft2_c(input):
