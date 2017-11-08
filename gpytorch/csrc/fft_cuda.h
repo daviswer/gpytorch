604a20916e23e9a2219ae4b5e672db594fd04c6d
daviswert <dww78@cornell.edu>
2017-11-07 21:37:42 -0500

    Removing cmul

--- gpytorch/csrc/fft_cuda.h
+++ gpytorch/csrc/fft_cuda.h
@@ -8,6 +8,3 @@ int fft2_c2r_cuda(THCudaTensor *input, THCudaTensor *output);
 int fft3_c2r_cuda(THCudaTensor *input, THCudaTensor *output);
 int ifft2_c2c_cuda(THCudaTensor *input, THCudaTensor *output);
 
-void ComplexPointwiseMulAndScale (THCudaTensor *a, THCudaTensor *b, int size);
-
-int cmul_cuda(THCudaTensor *input, THCudaTensor *output);
