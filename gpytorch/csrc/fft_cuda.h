638c2370fd7550ab443ae73e779f125075c96119
daviswert <dww78@cornell.edu>
2017-11-10 23:05:47 -0500

    Adding fftc

--- gpytorch/csrc/fft_cuda.h
+++ gpytorch/csrc/fft_cuda.h
@@ -2,6 +2,7 @@ int fft1_r2c_cuda(THCudaTensor *input, THCudaTensor *output);
 int fft2_r2c_cuda(THCudaTensor *input, THCudaTensor *output);
 int fft3_r2c_cuda(THCudaTensor *input, THCudaTensor *output);
 int fft2_c2c_cuda(THCudaTensor *input, THCudaTensor *output);
+int fftc_r2c_cuda(THCudaTensor *input, THCudaTensor *output);
 
 int fft1_c2r_cuda(THCudaTensor *input, THCudaTensor *output);
 int fft2_c2r_cuda(THCudaTensor *input, THCudaTensor *output);
