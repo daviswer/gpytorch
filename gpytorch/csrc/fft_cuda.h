cc40433080f6526a706672e5f76c0a212b3b0500
daviswert <dww78@cornell.edu>
2017-12-03 23:39:38 -0500

    fftc now complex

--- gpytorch/csrc/fft_cuda.h
+++ gpytorch/csrc/fft_cuda.h
@@ -2,11 +2,11 @@ int fft1_r2c_cuda(THCudaTensor *input, THCudaTensor *output);
 int fft2_r2c_cuda(THCudaTensor *input, THCudaTensor *output);
 int fft3_r2c_cuda(THCudaTensor *input, THCudaTensor *output);
 int fft2_c2c_cuda(THCudaTensor *input, THCudaTensor *output);
-int fftc_r2c_cuda(THCudaTensor *input, THCudaTensor *output);
+int fftc_c2c_cuda(THCudaTensor *input, THCudaTensor *output);
 
 int fft1_c2r_cuda(THCudaTensor *input, THCudaTensor *output);
 int fft2_c2r_cuda(THCudaTensor *input, THCudaTensor *output);
 int fft3_c2r_cuda(THCudaTensor *input, THCudaTensor *output);
 int ifft2_c2c_cuda(THCudaTensor *input, THCudaTensor *output);
-int fftc_c2r_cuda(THCudaTensor *input, THCudaTensor *output);
+int ifftc_c2c_cuda(THCudaTensor *input, THCudaTensor *output);
 
